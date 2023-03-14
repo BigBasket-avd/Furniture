036ec7788f8265ca29857e18f3e5fb4ad825bf82
Pritam <Pritam@gmail.com>
2023-03-14 01:53:29 +0000

    Committed with Dev

--- Product.java
+++ Product.java
@@ -5,6 +5,8 @@ class Product
 		   System.out.println("My Furniture World")
 		   System.out.println("I want to buy and sell chairs")
 		   System.out.println("Hello")
+		   System.out.println("code changed in Dev Branch")
+		   
 
 	   }
 
