a7a036374402d01aeffebf42625b00640a2bcc60
Pritam <Pritam@gmail.com>
2023-03-14 02:19:15 +0000

    Committed in Master Branch

--- Product.java
+++ Product.java
@@ -5,6 +5,7 @@ class Product
 		   System.out.println("My Furniture World")
 		   System.out.println("I want to buy and sell chairs")
 		   System.out.println("Hello")
+		   System.out.println("Hi")
 
 	   }
 
