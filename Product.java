e9bcacc0366d4b5df1cae9dfbc862f77f20163c3
amita-avd <127201794+amita-avd@users.noreply.github.com>
2023-03-21 22:37:49 +0530

    Update Product.java
    Changed by Amita on 21st March

--- Product.java
+++ Product.java
@@ -6,6 +6,7 @@ class Product
 		   System.out.println("I want to buy and sell chairs")
 		   System.out.println("Hello")
 		   System.out.println("code changed in Dev Branch")
+			system.out.println("Hi Samarjeet, How r yu ?"
 		   
 
 	   }
